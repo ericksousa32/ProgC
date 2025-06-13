96ff32cce866ac8d3d65923f2a21e04ad9fb708f
ericksousa32 <erickwilliams879@gmail.com>
2025-06-13 10:26:00 -0300

    Update matricula_aluno.c

--- media.c
+++ media.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main(){
+  int idade, matricula;
+  float altura;
+  chat nome[50];
+
+  printf("Digite a sua idade: \n");
+  scanf("%d", &idade);
+  
+  printf("Digite a sua altura: \n");
+  scanf("%f", &altura);
+
+  printf("Digite o seu nome: \n");
+  scanf(" %s", nome);
+
+  printf("Digite a sua matricula: ");
+  scanf("%d", &matricula);
+
+  printf("Nome do aluno: %s - Matricula: %d - Idade: %d - Altura: %.2f\n", nome, matricula, idade, altura);
+
+  return 0;
+}
